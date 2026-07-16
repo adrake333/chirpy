37b82a1ee80c3476a6be6a69ba5134dcd2fd6a0e
adrake333 <adrake333@gmail.com>
2026-07-16 09:31:23 -0500

    made and tested makeJWT and validateJWT

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,4 +6,55 @@ package auth
 import (
 	"testing"
 	"time"
+
+	"github.com/google/uuid"
 )
+
+
+
+
+func TestTokenValid(t *testing.T) {
+	originalID := uuid.New()
+	secret := "test-secret"
+	expiresIn := 1 * time.Hour
+	tokenString, err := MakeJWT(originalID, secret, expiresIn)
+	if err != nil {
+		t.Fatalf("expected no error creating JWT: %v", err)
+	}
+	gotID, err := ValidateJWT(tokenString, secret)
+	if err != nil {
+		t.Fatalf("expected no error validating JWT: %v", err)
+	}
+	if gotID != originalID {
+		t.Errorf("expected ID %v, got %v", originalID, gotID)
+	}
+}
+
+func TestExpired(t *testing.T) {
+	originalID := uuid.New()
+	secret := "test-secret"
+	expiresIn := -1 * time.Hour
+	tokenString, err := MakeJWT(originalID, secret, expiresIn)
+	if err != nil {
+		t.Fatalf("expected no error creating JWT: %v", err)
+	}
+	_, err = ValidateJWT(tokenString, secret)
+	if err == nil {
+		t.Fatal("expected error validating expired token")
+	}
+}
+
+func TestInvalidSigned(t *testing.T) {
+	originalID := uuid.New()
+	createSecret := "test-secret"
+	validateSecret := "wrong-secret"
+	expiresIn := 1 * time.Hour
+	tokenString, err := MakeJWT(originalID, createSecret, expiresIn)
+	if err != nil {
+		t.Fatalf("expected no error creating JWT: %v", err)
+	}
+	_, err = ValidateJWT(tokenString, validateSecret)
+	if err == nil {
+		t.Fatal("expected error validating token secret")
+	}
+}
